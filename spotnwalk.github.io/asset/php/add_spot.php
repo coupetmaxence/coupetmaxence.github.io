8a1a0ace1ea185fb9b22fa33477cafd6fd4ccf5e
Maxenc3 <maxence.coupet@gmail.com>
2017-02-16 15:43:59 +0100

    supression du code problématique

--- spotnwalk.github.io/asset/php/add_spot.php
+++ spotnwalk.github.io/asset/php/add_spot.php
@@ -9,16 +9,6 @@
 	$longitude_deg = $_POST["longitude_deg"];
 	$longitude_min = $_POST["longitude_min"];
 	$longitude_sec = $_POST["longitude_sec"];
-	$mysql_qry = "insert into spots (nom, categorie, description, latitude_deg, latitude_min, latitude_sec,
-					longitude_deg, longitude_min, longitude_sec) values ('$nom_spot','$categorie', '$description', 
-					'$latitude_deg', '$latitude_min', '$latitude_sec', '$longitude_deg', '$longitude_min', '$longitude_sec')";
-	if($conn->query($mysql_qry) === TRUE)
-	{
-		echo "ok";
-	}
-	else
-	{
-		echo "not ok";
-	}
+	
 	$conn->close();
 ?>
