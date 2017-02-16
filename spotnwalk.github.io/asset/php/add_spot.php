9508a077ad22959d4251f77ca1861cb5e8ce6fa2
Maxenc3 <maxence.coupet@gmail.com>
2017-02-16 15:47:04 +0100

    ters

--- spotnwalk.github.io/asset/php/add_spot.php
+++ spotnwalk.github.io/asset/php/add_spot.php
@@ -1,14 +1,6 @@
 <?php
 	
-	$nom_spot = $_POST["nom_spot"];
-	$categorie = $_POST["categorie"];
-	$description = $_POST["description"];
-	$latitude_deg = $_POST["latitude_deg"];
-	$latitude_min = $_POST["latitude_min"];
-	$latitude_sec = $_POST["latitude_sec"];
-	$longitude_deg = $_POST["longitude_deg"];
-	$longitude_min = $_POST["longitude_min"];
-	$longitude_sec = $_POST["longitude_sec"];
+	echo "le code php fonctionne";
 	
 
 ?>
